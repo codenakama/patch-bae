c17fde4d7f181940e313d82b54de77049e9a3e5e
Ricardo Abreu <rj.abreu89@gmail.com>
2018-07-24 13:14:55 +0100

    ui improvements card

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,56 @@
 import Link from 'next/link';
 import React from 'react';
-import { BackgroundImage, Box, Card, Flex, Subhead } from 'rebass';
+import {
+  Absolute,
+  BackgroundImage,
+  Box,
+  Card,
+  Flex,
+  Heading,
+  Relative
+} from 'rebass';
+import styled from 'styled-components';
+
+const Skrim = styled.div`
+  background: #000;
+`;
+
+const StyledHeading = styled(Heading)`
+  background-color: #d84263;
+  -webkit-box-decoration-break: clone;
+  box-decoration-break: clone;
+  color: #fff;
+`;
 
 const Index = props => {
   return (
     <div>
-      <Subhead mb={3}>Synths</Subhead>
+      <Heading fontSize={6} mb={4} color="#18A1A6">
+        Synths
+      </Heading>
+
       <Flex>
         <Box w={1 / 2} pr={3}>
           <Link href="/deepmind-12">
             <a>
-              <Card width={256} p={0}>
-                <BackgroundImage
-                  ratio={1 / 2}
-                  src="https://images.unsplash.com/photo-1523297554394-dc159677ffe1?ixlib=rb-0.3.5&s=bc1b389446b07d0729cf182f89d22587&auto=format&fit=crop&w=500&q=80"
-                />
-                <Subhead p={2}>Deepmind 12</Subhead>
-              </Card>
+              <Relative>
+                <Card width={256} p={0}>
+                  <BackgroundImage
+                    bg="#D84263"
+                    ratio={1 / 2}
+                    src="https://images.unsplash.com/photo-1523297554394-dc159677ffe1?ixlib=rb-0.3.5&s=bc1b389446b07d0729cf182f89d22587&auto=format&fit=crop&w=500&q=80"
+                  />
+                </Card>
+                <Absolute
+                  bottom={0}
+                  left={0}
+                  style={{ borderRadius: '4px', overflow: 'hidden' }}
+                >
+                  <StyledHeading p={2}>Deepmind 12</StyledHeading>
+                </Absolute>
+              </Relative>
             </a>
           </Link>
-        </Box>
-        <Box w={1 / 2}>
-          <Card width={256} p={0}>
-            <BackgroundImage
-              ratio={1 / 2}
-              src="https://images.unsplash.com/photo-1462331940025-496dfbfc7564?w=2048&q=20"
-            />
-            <Subhead p={2}>Hello</Subhead>
-          </Card>
         </Box>
       </Flex>
     </div>
