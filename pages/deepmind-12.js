9771c18f3eb5dc16d647e1d71cce52f26075f4dc
Ricardo Abreu <rj.abreu89@gmail.com>
2018-07-24 01:01:26 +0100

    refactor to class

--- pages/deepmind-12.js
+++ pages/deepmind-12.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Column, Row, Subhead, Text } from 'rebass';
+import { Column, Heading, Input, Row, Subhead, Text } from 'rebass';
 import {
   bankA,
+  bankB,
   bankC,
   bankD,
   bankE,
@@ -10,42 +11,69 @@ import {
   bankH
 } from './deepmind12/banks';
 
-const DeepMind12Page = props => {
-  return (
-    <div>
-      <Row wrap>
-        <Column>
-          <Subhead mb={3}>Bank A</Subhead>
-          {bankA.map(item => <Text mb={1}>{item}</Text>)}
-        </Column>
-        <Column>
-          <Subhead mb={3}>Bank C</Subhead>
-          {bankC.map(item => <Text mb={1}>{item}</Text>)}
-        </Column>
-        <Column>
-          <Subhead mb={3}>Bank D</Subhead>
-          {bankD.map(item => <Text mb={1}>{item}</Text>)}
-        </Column>
-        <Column>
-          <Subhead mb={3}>Bank E</Subhead>
-          {bankE.map(item => <Text mb={1}>{item}</Text>)}
-        </Column>
-        <Column>
-          <Subhead mb={3}>Bank F</Subhead>
-          {bankF.map(item => <Text mb={1}>{item}</Text>)}
-        </Column>
-        <Column>
-          <Subhead mb={3}>Bank G</Subhead>
-          {bankG.map(item => <Text mb={1}>{item}</Text>)}
-        </Column>
-        <Column>
-          <Subhead mb={3}>Bank H</Subhead>
-          {bankH.map(item => <Text mb={1}>{item}</Text>)}
-        </Column>
-      </Row>
-    </div>
-  );
-};
+class DeepMind12Page extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchFilter: ''
+    };
+  }
+
+  handleSearchChange = value => {
+    this.setState({ searchFilter: value });
+  };
+
+  render() {
+    const { props } = this;
+
+    return (
+      <div>
+        <Heading mb={3}>Deepmindg 12 Patch List</Heading>
+        <Row mb={3}>
+          <Input
+            placeholder="Search patch"
+            onChange={e => this.handleSearchChange(e.target.value)}
+          />
+        </Row>
+        <Row wrap>
+          <Column>
+            <Subhead mb={3}>Bank A</Subhead>
+            {bankA.map(item => <Text mb={1}>{item}</Text>)}
+          </Column>
+          <Column>
+            <Subhead mb={3}>Bank B</Subhead>
+            {bankB.map(item => <Text mb={1}>{item}</Text>)}
+          </Column>
+          <Column>
+            <Subhead mb={3}>Bank C</Subhead>
+            {bankC.map(item => <Text mb={1}>{item}</Text>)}
+          </Column>
+          <Column>
+            <Subhead mb={3}>Bank D</Subhead>
+            {bankD.map(item => <Text mb={1}>{item}</Text>)}
+          </Column>
+          <Column>
+            <Subhead mb={3}>Bank E</Subhead>
+            {bankE.map(item => <Text mb={1}>{item}</Text>)}
+          </Column>
+          <Column>
+            <Subhead mb={3}>Bank F</Subhead>
+            {bankF.map(item => <Text mb={1}>{item}</Text>)}
+          </Column>
+          <Column>
+            <Subhead mb={3}>Bank G</Subhead>
+            {bankG.map(item => <Text mb={1}>{item}</Text>)}
+          </Column>
+          <Column>
+            <Subhead mb={3}>Bank H</Subhead>
+            {bankH.map(item => <Text mb={1}>{item}</Text>)}
+          </Column>
+        </Row>
+      </div>
+    );
+  }
+}
 
 DeepMind12Page.propTypes = {};
 
